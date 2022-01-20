5201963b0f08cf14b4d19e4e967f7cc5a9e2c721
MaAamie <benoit.verva@gmail.com>
2022-01-20 22:19:00 +0100

    update sensor

--- custom_components/earnapphaview/sensor.py
+++ custom_components/earnapphaview/sensor.py
@@ -1,10 +1,10 @@
 """ import earnapp """
 
-from earnapp import earnapp
+import earnapp
 
-CONF_TOKEN = ""
+toke = ''
 
-""" impot HA """
+""" import HA """
 #import homeassistant.helpers.config_validation as cv
 #from homeassistant.components.sensor import PLATFORM_SCHEMA
 #from homeassistant.const import (
@@ -13,7 +13,7 @@ CONF_TOKEN = ""
 
 """ const """
 
-from .const import (
+from const import (
     DOMAIN,
     __name__,
 )
@@ -30,51 +30,64 @@ from .const import (
 """ Objet """
 
 class EarnAppObject:
+    def __init__(self):
+        self.euser = earnapp.User()
 
-    def __init__(self, CONF_TOKEN):
-		self.euser = earnapp.User()
-		self.DataOK = self.euser.login(self.CONF_TOKEN)
+    def updatetoken(self, token):
+        self.dataok = self.euser.login(token)
+        print(self.dataok)
 
-	def makeinfo(self):
-		if(self.DataOK):
-			self.Money = self.DataOK.money()
-			self.UserData = self.DataOK.userData()
-			#self.Devices = self.DataOK.devices()
-			self.AppVersions = self.DataOK.appVersions()
-			self.PaymentMethods = self.DataOK.paymentMethods()
-			self.Transactions = self.DataOK.transactions()
-			#self.ShowDevice = self.DataOK.showDevice()
-			print("Successfully logged in!")
-		else:
-			print("Failed to log in")
-	
-	def prints(self):
-		print("---------- Money ----------")
-		for cle, valeur in self.Money.items():
-    		print(cle + " Valeur : " + str(valeur))
+    def makeinfo(self):
+        if(self.dataok):
+            self.Money = self.euser.money()
+            self.UserData = self.euser.userData()
+            self.Devices = self.euser.devices()
+            self.AppVersions = self.euser.appVersions()
+            self.PaymentMethods = self.euser.paymentMethods()
+            self.Transactions = self.euser.transactions()
+            #self.ShowDevice = self.euser.showDevice()
+            print("Successfully logged in!")
+        else:
+            print("Failed to log in")
+    
+    def prints(self):
+        
+        print("---------- Money ----------")
+        for cle, valeur in self.Money.items():
+            print(cle + " Valeur : " + str(valeur))
 
-		print("---------- UserData ----------")
-		for cle, valeur in self.UserData.items():
-    		print(cle + " Valeur : " + str(valeur))
+        print("---------- UserData ----------")
+        for cle, valeur in self.UserData.items():
+            print(cle + " Valeur : " + str(valeur))
 
-		print("---------- Devices ----------")
-		#for cle, valeur in self.Devices.items():
-		#    print(cle + " Valeur : " + str(valeur))
+        print("---------- Devices ----------")
+        print(self.Devices)
+        #for cle, valeur in self.Devices:
+        #    print(cle + " Valeur : " + str(valeur))
 
-		print("---------- AppVersions ----------")
-		for cle, valeur in self.AppVersions.items():
-    		print(cle + " Valeur : " + str(valeur))
+        print("---------- AppVersions ----------")
+        for cle, valeur in self.AppVersions.items():
+            print(cle + " Valeur : " + str(valeur))
 
-		print("---------- PaymentMethods ----------")
-		for cle, valeur in self.PaymentMethods.items():
-    		print(cle + " Valeur : " + str(valeur))
-	
-		#print("---------- Transactions ----------")
-		#for cle, valeur in self.Transactions.items():
-		#    print(cle + " Valeur : " + str(valeur))
+        print("---------- PaymentMethods ----------")
+        for cle, valeur in self.PaymentMethods.items():
+            print(cle + " Valeur : " + str(valeur))
+    
+        print("---------- Transactions ----------")
+        print(self.Transactions)
 
-		print("---------- ShowDevice ----------")
-		#for cle, valeur in self.ShowDevice.items():
-		#    print(cle + " Valeur : " + str(valeur))
+        print("---------- ShowDevice ----------")
+        #print(self.ShowDevice())
+        #for cle, valeur in self.ShowDevice.items():
+        #    print(cle + " Valeur : " + str(valeur))
 
-EarnAppObject.
+
+
+
+earnobj = EarnAppObject()
+
+earnobj.updatetoken(toke)
+
+earnobj.makeinfo()
+
+earnobj.prints()
